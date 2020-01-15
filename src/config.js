a7f858409d234966b7b50e7c9b1ad074b16f3d91
Виктор Кондик <strunoder24@mail.ru>
2020-01-15 13:45:27 +1000

    all 4 operations is working

--- src/config.js
+++ src/config.js
@@ -1,3 +1,115 @@
 export default {
-    key: 'value'
+    modes: [
+        {
+            enable: true,
+            operationName: 'Посчитать сумму',
+            operationSymbol: '+',
+            
+            calculateResult (){
+                if (this.validator()) this.result = parseInt(this.input1) + parseInt(this.input2)
+            },
+            
+            validator(){
+                const firstNumber = this.input1.toString();
+                const secondNumber = this.input2.toString();
+                
+                if (firstNumber.length === 0 || secondNumber.length === 0) {
+                    alert('Пожалуйста, укажите оба числа');
+                    return false
+                }
+    
+                this.error = '';
+                return true
+            },
+        },
+    
+        {
+            enable: true,
+            operationName: 'Посчитать разность',
+            operationSymbol: '-',
+        
+            calculateResult (){
+                if (this.validator()) this.result = parseInt(this.input1) - parseInt(this.input2)
+            },
+        
+            validator(){
+                const firstNumber = this.input1.toString();
+                const secondNumber = this.input2.toString();
+                
+                if (firstNumber.length === 0 || secondNumber.length === 0) {
+                    alert('Пожалуйста, укажите оба числа');
+                    return false
+                }
+    
+                this.error = '';
+                return true
+            },
+        },
+    
+        {
+            enable: true,
+            operationName: 'Посчитать произведение',
+            operationSymbol: '*',
+        
+            calculateResult (){
+                if (this.validator()) this.result = parseInt(this.input1) * parseInt(this.input2)
+            },
+        
+            validator(){
+                const firstNumber = this.input1.toString();
+                const secondNumber = this.input2.toString();
+                
+                if (firstNumber.length === 0 || secondNumber.length === 0) {
+                    alert('Пожалуйста, укажите оба числа');
+                    return false
+                }
+                
+                else if (firstNumber === '0' || secondNumber === '0') {
+                    this.error = 'Умножение любого числа на ноль, всегда равно нолю';
+                    return true
+                }
+
+                else if (firstNumber === '1' || secondNumber === '1') {
+                    this.error = 'При умножении любого числа на единицу результат будет равен этому числу';
+                    return true
+                }
+    
+                this.error = '';
+                return true
+            },
+        },
+    
+        {
+            enable: true,
+            operationName: 'Посчитать разность',
+            operationSymbol: '/',
+        
+            calculateResult (){
+                if (this.validator()) this.result = parseInt(this.input1) / parseInt(this.input2)
+            },
+        
+            validator(){
+                const firstNumber = this.input1.toString();
+                const secondNumber = this.input2.toString();
+                
+                if (firstNumber.length === 0 || secondNumber.length === 0) {
+                    alert('Пожалуйста, укажите оба числа');
+                    return false
+                }
+            
+                else if (firstNumber === '0' && secondNumber !== '0') {
+                    this.error = 'Деление ноля на любое число, всегда равно нолю';
+                    return true
+                }
+
+                else if (secondNumber === '0') {
+                    alert('На ноль делить нельзя');
+                    return false
+                }
+            
+                this.error = '';
+                return true
+            },
+        },
+    ]
 }
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,17 +1,26 @@
 <template>
     <div class='container'>
         <input v-model='input1'
-               @input='calculateResult()'
                min='0'
+               @click='focusInput($event)'
+               @keydown.enter='enterKeyHandler()'
                type='number'>
         <span class='arithmetic-symbol'>{{ operationSymbol }}</span>
         <input v-model='input2'
-               @input='calculateResult()'
                min='0'
+               @click='focusInput($event)'
+               @keydown.enter='enterKeyHandler()'
                type='number'>
         <span class='arithmetic-symbol'>=</span>
         <span class='result'>{{ getResult(result) }}</span>
         <div class='error' v-if='error'>{{ error }}</div>
+        <div class='buttons-container'>
+            <button v-for='button in buttons'
+                    @click='handleButtonPress(button)'
+            >
+                {{ button.operationName }}
+            </button>
+        </div>
     </div>
 </template>
 
@@ -23,49 +32,152 @@
         data: () => ({
             input1: 0,
             input2: 0,
-            operationSymbol: '+',
             result: 0,
-            error: ''
+            error: '',
+    
+            operationSymbol: '+',
+            buttons: [
+                {
+                    operationSymbol: '+',
+                    operationName: 'Посчитать сумму',
+                }
+            ],
+            hasExternalHandlers: false
         }),
-        mounted() {
         
+        mounted() {
+            this.init();
         },
-        computed: {},
+        
         methods: {
+            // Расчётная функция по умолчанию
             calculateResult(){
-                if (this.validate()) this.result = parseInt(this.input1) + parseInt(this.input2)
+                if (this.validator()) this.result = parseInt(this.input1) + parseInt(this.input2)
             },
             
-            validate(){
-                if (this.input1.toString().length && this.input2.toString().length) {
-                    this.error = '';
-                    return true
-                } else {
-                    this.error = 'please set both numbers';
+            // Валидатор по умолчанию
+            validator(){
+                const firstNumber = this.input1.toString();
+                const secondNumber = this.input2.toString();
+    
+                if (firstNumber.length === 0 || secondNumber.length === 0) {
+                    alert('Пожалуйста, укажите оба числа');
                     return false
+                }
+    
+                this.error = '';
+                return true
+            },
+            
+            // Обработка нажатия на кнопку "посчитать"
+            handleButtonPress(button) {
+                if (this.hasExternalHandlers) {
+                    // если имеется хотя бы один внешний хэндлер, то находим его по символу операции
+                    let handler = config.modes.find(el => {
+                        return el.operationSymbol === button.operationSymbol;
+                    });
+    
+                    // Перезаписываем обработчики в компоненте
+                    this.calculateResult = handler.calculateResult;
+                    this.validator = handler.validator;
+                    this.operationSymbol = handler.operationSymbol;
+                    
+                    this.calculateResult();
+                    
+                } else {
+                    // Если нет активных внешних хэндлеров
+                    this.calculateResult();
                 }
             },
     
+            // Вспомогательная функция для лучшего UX
             getResult(){
-                return divideByThousands(this.result)
+                if (typeof this.result === 'number') return divideByThousands(this.result);
+                else return this.result;
+            },
+    
+            // Проверка есть ли активные хэндлеры во внешнем файле
+            hasAnyActiveHandlers(){
+                return config.modes.some((el) => {
+                    return el.enable
+                })
+            },
+    
+            enterKeyHandler(){
+                if (this.hasExternalHandlers) this.handleButtonPress(this.buttons.find(el => {
+                    return el.operationSymbol === this.operationSymbol;
+                }));
+                else this.calculateResult()
+            },
+    
+            // Вспомогательная функция для лучшего UX
+            focusInput(e) {
+                e.toElement.select();
+            },
+            
+            // Функция проверяет наличие внешнего файла с обработчиками
+            init() {
+                if (config && config.modes && this.hasAnyActiveHandlers()) {
+                    this.hasExternalHandlers = true;
+                    this.buttons = config.modes.filter(el => {
+                        return el.enable
+                    });
+                    this.operationSymbol = this.buttons[0].operationSymbol;
+                }
             }
         },
-        components: {}
     }
 </script>
 
 <style lang='scss' scoped>
     .arithmetic-symbol {
-        margin: 0 10px
+        display: inline-block;
+        width: 10px;
+        margin: 0 10px;
+        text-align: center;
     }
     
     
     input {
-        width: 60px
+        width: 60px;
+        border-radius: 5px;
+    }
+    
+    button {
+        background-color: transparent;
+        border: 1px solid dodgerblue;
+        border-radius: 5px;
+        font-size: 20px;
+    }
+    
+    
+    .result {
+        font-weight: bold;
     }
     
     
     .error {
-        color: red
+        color: red;
+        font-weight: bold;
+    }
+    
+    
+    .buttons-container {
+        padding: 20px 0
+    }
+    
+    button {
+        display: block;
+        transition: color linear .2s, background-color linear .2s;
+        cursor: pointer;
+        
+        &:not(:last-child) {
+            margin-bottom: 10px;
+        }
+        
+        &:hover {
+            color: #fff;
+            background-color: dodgerblue;
+        }
     }
 </style>
